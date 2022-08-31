87acfd74a5748531020ecbbbc766d86c3699e2f3
Antosha Baboshin <antosha.js@gmail.com>
2022-08-31 16:47:56 +0300

    feat: add document.title check in index.ts

--- index.ts
+++ index.ts
@@ -3,17 +3,18 @@ import Aut from "./src/api/aut";
 import { addPagination, addWordsList, checkActiveLevel } from "./src/components/ebook/ebook";
 import { defaultStorage } from "./src/utilities/defaultStorage";
 
-console.log('1');
+if (document.title === 'E-Book') {
+    const addEbook = () => {
+    defaultStorage();
+    checkActiveLevel();
+    addWordsList();
+    addPagination();
+  }
+  addEbook();
+}
 
-const aut = new Aut('https://sveta077-rslang.herokuapp.com');
-aut.addListener();
-aut.viewUser();
-
-// const addEbook = () => {
-//   defaultStorage();
-//   checkActiveLevel();
-//   addWordsList();
-//   addPagination();
-// }
-
-// addEbook();
+if (document.title === 'RSLang') {
+  const aut = new Aut('https://sveta077-rslang.herokuapp.com');
+  aut.addListener();
+  aut.viewUser();
+}
