24fae04f7f6a590aa9726288509e977027a92828
pssofer <pssofer5@gmail.com>
2022-09-02 19:33:23 +0300

    feat: add header, footer, sidebar, authorization styles

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import Aut from "./src/api/aut";
 import './src/css/base.css'
 import './src/css/main.css'
 import './src/css/reset.css'
+import './src/css/sidebar.css'
+import './src/css/header.css'
+import './src/css/authorization.css'
+
 
 const aut = new Aut('https://sveta077-rslang.herokuapp.com');
 aut.addListener();
