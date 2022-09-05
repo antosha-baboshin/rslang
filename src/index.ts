ff3ff2b2ca65209eec82a91d5a0e9ce2814450e3
Sveta077 <pugach.svetka@gmail.com>
2022-09-05 09:28:25 +0200

    feat: changes for showing authorization-user in main-page; small cosmetic changes

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,11 @@ aut.loadUser()
 const profile_icon= document.getElementById("profile_icon") as HTMLInputElement;
 if (profile_icon && aut.avatara!='') profile_icon.src=aut.avatara
 
-document.getElementById('stsprint')!
-  .addEventListener("click", ()=>window.location.href =`./sprint/sprint-main.html` )  
+const show_name= document.getElementById("showname") as HTMLInputElement;
+  if (show_name && aut.name!='') show_name.innerText=aut.name
 
-  document.getElementById('stchallenge')!
-  .addEventListener("click", ()=>window.location.href =`./challenge/challenge-main.html` )  
+document.getElementById("usrsignout")!.addEventListener("click",()=>{aut.SignOut();
+  profile_icon.src='./assets/img/header-img.jpeg'
+  show_name.innerText='Log In'
+})
 
-  document.getElementById('statistics')!
-  .addEventListener("click", ()=>window.location.href =`./statistic/statistic.html` )  
-
-
-console.log('1');
